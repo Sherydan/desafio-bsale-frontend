c26f5ea1a48ab7d9b6e80cbc1498078bc760625f
Sherydan <tobar.luis.c@gmail.com>
2022-07-17 22:32:41 -0400

    listing products by category

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+let categoriesList = document.querySelector("#categoriesList");
+
 const listProductsOffers= async () => {
     response = await fetch("http://localhost:4000/api/products/offers");
     const products = await response.json();
@@ -25,6 +27,43 @@ const listProductsOffers= async () => {
     document.querySelector("#productsRowContainer").innerHTML = content;
     });
 }
+
+const listProductsByCategory = async (id) => {
+    response = await fetch("http://localhost:4000/api/products/category/" + id);
+    const products = await response.json();
+    document.querySelector("#productsRowContainer").innerHTML = " ";
+    let content = ``;
+
+    products.forEach( (product, index) => {
+        content += `
+        <div class="col-md-3 my-2">
+          <div class="card">
+              <img src="${product.url_image}" class="card-img-top img-fluid resize" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">$${product.price}</p>
+                <p class="card-text">Descuento${product.discount}%</p>
+
+                <div class="text-center">
+                <a href="#" class="btn btn-primary">Agregar</a>
+                </div>
+                
+              </div>
+            </div>
+        </div>`;
+
+    document.querySelector("#productsRowContainer").innerHTML = content;
+    });
+}
+
+
+categoriesList.addEventListener("click", (e) => {
+    let categoryId = e.target.dataset.categoryid;
+    listProductsByCategory(categoryId);
+
+});
+
+
 
 window.addEventListener("load", function () {
     console.log("documento cargado");
